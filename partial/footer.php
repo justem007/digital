fd6909658a7cc70f84d0d77094bdd6a7a5565bd3
justem007 <justem007@hotmail.com>
2016-01-28 16:03:56 -0200

    digital commit 50

--- partial/footer.php
+++ partial/footer.php
@@ -237,7 +237,7 @@
         iframe: {
             markup: '<div class="mfp-iframe-scaler">'+
             '<div class="mfp-close"></div>'+
-            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+            '<iframe class="mfp-iframe" frameborder="5" allowfullscreen></iframe>'+
             '<div class="mfp-title">Seu vídeo esta sendo carregado favor aguardar ......</div>'+
             '</div>'
         },
