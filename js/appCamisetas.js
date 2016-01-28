af7ffad42134549e2be9d462b7848cb044df1b98
justem007 <justem007@hotmail.com>
2016-01-28 14:24:09 -0200

    digital commit 47

--- js/appCamisetas.js
+++ js/appCamisetas.js
@@ -1,4 +1,4 @@
 angular.module('Digital', ['angular-loading-bar', 'ngAnimate'])
     .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
-        cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Loading...</div>';
+        cfpLoadingBarProvider.includeSpinner = false;
     }]);
