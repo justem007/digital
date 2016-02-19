e10b56d88457643c74f72035919bf586e5bbeb82
justem007 <justem007@hotmail.com>
2016-02-19 14:47:10 -0200

    commit formulario de agenda

--- pages-original/form.php
+++ pages-original/form.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Formulario de Visita</title>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
     <link rel="stylesheet" href="components/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="components/normalize-css/normalize.css">
     <link rel="stylesheet" href="components/app.css">
@@ -31,30 +32,9 @@
             <label for="Celular">Celular:</label>
             <input type="text" name="Celular" id="Celular" class="form-control" placeholder="seu celular" required/>
         </div>
-        <br>
-        <div class="medium-5 column labelade ">
-            <label for="gender" class="error">Seleciona um dia dessa semana</label><br>
-            <label class="radio-inline">
-                <input type="radio" name="DiaSemana" id="inlineRadio1" value="domingo">Domingo
-            </label><br>
-            <label class="radio-inline">
-                <input type="radio" name="DiaSemana" id="inlineRadio2" value="segunda">Segunda
-            </label><br>
-            <label class="radio-inline">
-                <input type="radio" name="DiaSemana" id="inlineRadio3" value="terça">Terça
-            </label><br>
-            <label class="radio-inline">
-                <input type="radio" name="DiaSemana" id="inlineRadio4" value="quarta">Quarta
-            </label><br>
-            <label class="radio-inline">
-                <input type="radio" name="DiaSemana" id="inlineRadio5" value="quinta">Quinta
-            </label><br>
-            <label class="radio-inline">
-                <input type="radio" name="DiaSemana" id="inlineRadio6" value="sexta">Sexta
-            </label><br>
-            <label class="radio-inline">
-                <input type="radio" name="DiaSemana" id="inlineRadio7" value="sábado">Sábado
-            </label>
+        <div class="form-group">
+            <label for="Celular">Selecione dia e mês. Visitas todos os dias e fins de semana.</label>
+            <input type="text" name="DiaSemana" id="calendario" class="form-control" placeholder="escolha a data">
         </div>
         <hr>
         <div class="medium-5 columns labelade">
@@ -72,7 +52,6 @@
                 <input type="radio" name="Turno" id="inlineRadio4" value="de 15:00 as 17:00">de 15:00 as 17:00
             </label>
         </div>
-        <br>
         <hr>
         <div class="medium-5 columns labelade">
             <label for="gender" class="error">Quais os produtos você deseja obter atendimento ?</label><br>
@@ -80,19 +59,19 @@
                 <input type="checkbox" name="Atendimento[]" id="logistica" value="logistica">logística.
             </label><br>
             <label class="checkbox-inline">
-                <input type="checkbox" name="Atendimento[]" id="camisetas" value="camisetas">camisetas.
+                <input type="checkbox" name="Atendimento[]" id="camisetas" value="camisetas ou revendas">camisetas ou revendas.
             </label><br>
             <label class="checkbox-inline">
                 <input type="checkbox" name="Atendimento[]" id="revenda-tecidos" value="revenda de tecidos">revenda de tecidos.
             </label><br>
             <label class="checkbox-inline">
-                <input type="checkbox" name="Atendimento[]" id="comprar-tecidos-estampas" value="comprar de tecidos ou estampas">compra de tecidos ou estampas.
+                <input type="checkbox" name="Atendimento[]" id="comprar-tecidos-estampas" value="comprar tecidos ou estampas">compra de tecidos ou estampas.
             </label><br>
             <label class="checkbox-inline">
                 <input type="checkbox" name="Atendimento[]" id="servicos-terceirizacao" value="serviços e terceirização">serviços e terceirização.
             </label>
         </div>
-        <br><br>
+        <br>
         <div class="columns input txt">
             <label for="Mensagem">Mensagem: </label>
             <textarea name="Mensagem" rows="4" cols="80" class="form-control">
@@ -106,7 +85,7 @@
         <input type="submit" name="Enviar" value="Enviar" class="btn btn-primary btn-lg submit buttons"/>
         <hr>
         <button type="button" class="btn btn-default btn-lg visible-xs"><a href="index.php">Voltar - Home</a></button>
-        <br>
+        <button type="button" class="btn btn-default btn-lg visible-sm"><a href="index.php">Voltar - Home</a></button>
     </form>
     <script src="dist/form.js"></script>
 </div>
