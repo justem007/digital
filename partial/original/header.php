7cb657a70ad387204856d02b810debf001226572
justem007 <justem007@hotmail.com>
2016-03-30 08:39:04 -0300

    colocando o chat no ar

--- partial/original/header.php
+++ partial/original/header.php
@@ -37,6 +37,17 @@
             border:none;
         }
     </style>
+
+    <!--Start of Zopim Live Chat Script-->
+    <script type="text/javascript">
+        window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+            d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+        _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+            $.src="//v2.zopim.com/?3XaCdlRGe1AUsvDN1Rry3Z6PX2x8d2TX";z.t=+new Date;$.
+                type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+    </script>
+    <!--End of Zopim Live Chat Script-->
+
 </head>
 <body class="fadeIn">
 <nav id="meuMenu">
