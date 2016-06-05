277119b4f8721b0db5c39c5b564b94df9d962521
justem007 <justem007@hotmail.com>
2016-06-05 18:49:35 -0300

    acertando fraze das imagens

--- partial/main.php
+++ partial/main.php
@@ -5,7 +5,7 @@
 </div>
 <div class="row small-up-1 medium-up-1 large-up-2">
     <h4 class="text-center">
-        Conheça nossos produtos e serviços de impressão digital !!!<br>
+        Conheça nossos produtos e serviços de impressão digital textil !!!<br>
         Atendimento exclusivo, <a href="form.php" style="text-decoration: underline">agende agora uma visita</a> na fábrica.
     </h4>
     <br>
