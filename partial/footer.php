55091846b18dc68e9d7c40b3726cc05cced3880c
Ricardo Justem <justem007@hotmail.com>
2016-06-30 08:25:15 -0300

    Update footer.php

--- partial/footer.php
+++ partial/footer.php
@@ -15,11 +15,11 @@
                 <span style="color: #0b0b0b">Email:</span> contato@rossinaestamparia.com.br
             </li><br>
             <h5 class="titulo" style="text-decoration: underline">Dados da Empresa</h5>
-            <li>NITISILK INDUSTRIA E COMERCIO DE ROUPAS LTDA - EPP</li>
-            <li>
-                <span style="color: #0b0b0b">Endereço:</span> Avenida Presidente Roosevelt, 130 parte
-            </li>
-            <li>
+            <li>NITISILK INDUSTRIA E COMERCIO DE ROUPAS LTDA - EPP<br>
+            
+                <span style="color: #0b0b0b">Endereço:</span> Avenida Presidente Roosevelt, 130 parte<br>
+            
+            
             Vista Alegre - São Gonçalo <br>
             Estado: Rio de Janeiro <br>
             Cep: 24722-070</li><br>
