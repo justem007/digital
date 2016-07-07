38a2b24c67f0615da19e63061a9b96c48b943963
justem007 <justem007@hotmail.com>
2016-07-07 17:01:32 -0300

    acertnado commit

--- deploy/index.php
+++ deploy/index.php
@@ -3,13 +3,13 @@ session_start();
 $token = "6720720054e9d24fbf6c20a831ff287e";
 $senhaAcesso = 'insert65@';
 
-if($_REQUEST['token'] == $token)
+if ($_REQUEST['token'] == $token)
     $tokenValido = true;
 
-if($_REQUEST['senhaAcesso'] == $senhaAcesso && empty($_SESSION['usuarioValido']))
+if ($_REQUEST['senhaAcesso'] == $senhaAcesso && empty($_SESSION['usuarioValido']))
     $_SESSION['usuarioValido'] = true;
 
-if($_REQUEST['sair'])
+if ($_REQUEST['sair'])
     unset($_SESSION['usuarioValido']);
 ?>
 <!DOCTYPE html>
@@ -22,24 +22,24 @@ if($_REQUEST['sair'])
 <body>
 <pre>
 	<?php
-    if($tokenValido) {
+    if ($tokenValido) {
         $exec = shell_exec("git pull origin master 2>&1");
         echo $exec;
 
-        $textoLog = PHP_EOL."Data: ".date(d."/".m."/".Y." - ".H.":".i.":".s);
-        $textoLog .= PHP_EOL.$exec;
+        $textoLog = PHP_EOL . "Data: " . date(d . "/" . m . "/" . Y . " - " . H . ":" . i . ":" . s);
+        $textoLog .= PHP_EOL . $exec;
 
         $arquivoLog = fopen('log.txt', 'a+');
         fwrite($arquivoLog, $textoLog);
         fclose($arquivoLog);
-    } else if($_SESSION['usuarioValido']) {
+    } else if ($_SESSION['usuarioValido']) {
         ?>
         <form action="index.php" method="post">
 			<input type="hidden" name="token" value="6720720054e9d24fbf6c20a831ff287e">
 			<input type="submit" value="Atualizar">
 		</form>
         <?php
-        if($_SESSION['usuarioValido'])
+        if ($_SESSION['usuarioValido'])
             echo '<p><a href="index.php?sair=true">Sair</a></p>';
         $texto = file('log.txt');
         foreach ($texto as $linha) {
@@ -47,12 +47,17 @@ if($_REQUEST['sair'])
         }
     } else {
         ?>
-        <form action="index.php" method="post">
+        <div class="container">
+            <div class="row">
+                <form action="index.php" method="post">
 			<div>
-				<input type="password" placeholder="Senha" name="senhaAcesso">
+				<input class="form-control" type="password" placeholder="Senha" name="senhaAcesso">
 			</div>
-			<input type="submit" value="Acessar Sistema">
+			<input class="btn btn-default" type="submit" value="Acessar Sistema">
 		</form>
+            </div>
+        </div>
+
         <?php
     }
     ?>
