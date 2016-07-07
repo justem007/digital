2f83cab11ed78b2856d80c4aa58a3fcc6ea3fb68
justem007 <justem007@hotmail.com>
2016-07-07 16:49:09 -0300

    acertnado commit

--- deploy/index.php
+++ deploy/index.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+$token = "6720720054e9d24fbf6c20a831ff287e";
+$senhaAcesso = 'insert65@';
+
+if($_REQUEST['token'] == $token)
+    $tokenValido = true;
+
+if($_REQUEST['senhaAcesso'] == $senhaAcesso && empty($_SESSION['usuarioValido']))
+    $_SESSION['usuarioValido'] = true;
+
+if($_REQUEST['sair'])
+    unset($_SESSION['usuarioValido']);
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,11 +19,42 @@
     <title>Usando GIT para atualizar arquivos no servidor de hospedagem</title>
 </head>
 <body>
-	<pre>
+<pre>
 	<?php
-    $exec = shell_exec("git pull origin master 2>&1");
-    echo $exec;
+    if($tokenValido) {
+        $exec = shell_exec("git pull origin master 2>&1");
+        echo $exec;
+
+        $textoLog = PHP_EOL."Data: ".date(d."/".m."/".Y." - ".H.":".i.":".s);
+        $textoLog .= PHP_EOL.$exec;
+
+        $arquivoLog = fopen('log.txt', 'a+');
+        fwrite($arquivoLog, $textoLog);
+        fclose($arquivoLog);
+    } else if($_SESSION['usuarioValido']) {
+        ?>
+        <form action="index.php" method="post">
+			<input type="hidden" name="token" value="6720720054e9d24fbf6c20a831ff287e">
+			<input type="submit" value="Atualizar">
+		</form>
+        <?php
+        if($_SESSION['usuarioValido'])
+            echo '<p><a href="index.php?sair=true">Sair</a></p>';
+        $texto = file('log.txt');
+        foreach ($texto as $linha) {
+            echo $linha;
+        }
+    } else {
+        ?>
+        <form action="index.php" method="post">
+			<div>
+				<input type="text" placeholder="Senha" name="senhaAcesso">
+			</div>
+			<input type="submit" value="Acessar Sistema">
+		</form>
+        <?php
+    }
     ?>
-	</pre>
+</pre>
 </body>
 </html>
