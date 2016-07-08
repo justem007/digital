c228dc0d33dd3f688343f83a94a743a623efe107
justem007 <justem007@hotmail.com>
2016-07-08 15:19:14 -0300

    criando as migrations

--- partial/header.php
+++ partial/header.php
@@ -1,4 +1,28 @@
-<!DOCTYPE html><html class="no-js" lang="pt-br" dir="etl"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><meta http-equiv="content-language" content="pt-br"><title><?php echo $title ?></title><meta name="google-site-verification" content="google1a06754d50be5d0d"><meta property="og:title" content="Rossina Estamparia Digital - Tecidos - Camisas - Camisetas - Serviços estamparia digital"><meta property="og:description" content="<?php echo $description; ?>"><meta property="og:site_name" content="Rossina Estamparia Digital"><meta property="og:url" content="https://rossinaestamparia.com.br/"><meta property="og:type" content="website"><meta property="og:image" content="https://rossinaestamparia.com.br/images/logo-rossina.png"><meta property="og:site_name" content="Rossina Estamparia Digital"><link rel="icon" type="image/x-icon" href="https://rossinaestamparia.com.br/favicon.ico"><link rel="icon" type="image/x-icon" href="/favicon.ico"><link rel="alternate" hreflang="pt-br" href="https://rossinaestamparia.com.br/"><link rel="stylesheet" href="dist/css/bundle.css" type="text/css"><link rel="stylesheet" href="css/foundation-icons/foundation-icons.css" type="text/css"><link rel="stylesheet" href="components/normalize-css/normalize.css" type="text/css"><link rel="stylesheet" href="components/font-awesome/css/font-awesome.min.css" type="text/css"><style>#meuMenu {
+<!DOCTYPE html>
+<html class="no-js" lang="pt-br" dir="etl">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="content-language" content="pt-br">
+    <title><?php echo $title ?></title>
+    <meta name="google-site-verification" content="google1a06754d50be5d0d" />
+    <meta property="og:title" content="Rossina Estamparia Digital - Tecidos - Camisas - Camisetas - Serviços estamparia digital">
+    <meta property="og:description" content="<?php echo $description; ?>">
+    <meta property="og:site_name" content="Rossina Estamparia Digital">
+    <meta property="og:url" content="https://rossinaestamparia.com.br/" />
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://rossinaestamparia.com.br/images/logo-rossina.png" />
+    <meta property="og:site_name" content="Rossina Estamparia Digital" />
+    <link rel="icon" type="image/x-icon" href="https://rossinaestamparia.com.br/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <link rel="alternate" hreflang="pt-br" href="https://rossinaestamparia.com.br/" />
+    <link rel="stylesheet" href="dist/css/bundle.css" type="text/css">
+    <link rel="stylesheet" href="css/foundation-icons/foundation-icons.css" type="text/css">
+    <link rel="stylesheet" href="components/normalize-css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="components/font-awesome/css/font-awesome.min.css" type="text/css">
+    <style>
+        #meuMenu {
             background:#069;
             width:100%;
             padding:5px;
@@ -20,8 +44,34 @@
         }
         .fi-grande3{
             width: 60px;
-        }</style><!--Start of Zopim Live Chat Script--><script type="text/javascript">window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+        }
+    </style>
+
+    <!--Start of Zopim Live Chat Script-->
+    <script type="text/javascript">
+        window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
             d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
         _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
             $.src="//v2.zopim.com/?3XaCdlRGe1AUsvDN1Rry3Z6PX2x8d2TX";z.t=+new Date;$.
-                type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");</script><!--End of Zopim Live Chat Script--></head><body class="fadeIn"><nav id="meuMenu"><div id="cssmenu"><ul><li class="show-for-medium"><a href="index.php"><img src="images/logo-rossina.png" width="150"></a></li><li><a href="index.php"><i class="fi-home fi-grande2"></i></a></li><li><a href="camisetas.php"><i class="fi-male-female fi-grande2"></i> Camisas</a></li><li><a href="tecidos.php"><i class="fi-photo fi-grande2"></i> Tecidos</a></li><li><a href="servicos.php"><i class="fi-paint-bucket fi-grande2"></i> Serviços</a></li><li><a href="silk-digital.php"><i class="fi-list fi-grande2"></i> Silk Digital</a></li><li><a href="videos-calandra-plotter-kornit-digital.php"><i class="fi-play-video fi-grande2"></i> Vídeos</a></li><li><a href="form.php"><i class="fi-page-multiple fi-grande2"></i> Agendar Visita</a></li><li><a href="#contatos" style="font-weight: bold; color: #00ff00"><i class="fi-page-multiple fi-grande2" style="font-weight: bold; color: #00ff00"></i> CONTATOS</a></li></ul></div></nav><br></body></html>
+                type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+    </script>
+    <!--End of Zopim Live Chat Script-->
+
+</head>
+<body class="fadeIn">
+<nav id="meuMenu">
+    <div id='cssmenu'>
+        <ul>
+            <li class="show-for-medium"><a href="index.php"><img src="images/logo-rossina.png" width="150"></a></li>
+            <li><a href="index.php"><i class="fi-home fi-grande2"></i></a></li>
+            <li><a href="camisetas.php"><i class="fi-male-female fi-grande2"></i> Camisas</a>
+            <li><a href="tecidos.php"><i class="fi-photo fi-grande2"></i> Tecidos</a></li>
+            <li><a href="servicos.php"><i class="fi-paint-bucket fi-grande2"></i> Serviços</a></li>
+            <li><a href="silk-digital.php"><i class="fi-list fi-grande2"></i> Silk Digital</a></li>
+            <li><a href="videos-calandra-plotter-kornit-digital.php"><i class="fi-play-video fi-grande2"></i> Vídeos</a></li>
+            <li><a href="form.php"><i class="fi-page-multiple fi-grande2"></i> Agendar Visita</a></li>
+            <li><a href="#contatos" style="font-weight: bold; color: #00ff00"><i class="fi-page-multiple fi-grande2" style="font-weight: bold; color: #00ff00"></i> CONTATOS</a></li>
+        </ul>
+    </div>
+</nav>
+<br>
